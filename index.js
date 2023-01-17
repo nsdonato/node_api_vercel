518e1a25cd630afe80043778a220e89fc845bb7c
noeliadv <noeliadv@ext.inditex.com>
2023-01-17 11:34:44 -0300

    test

--- index.js
+++ index.js
@@ -1,39 +1,20 @@
-const express = require("express")
-const app = express()
-const fs = require("fs")
-const multer = require("multer")
-const path = require("path")
-const upload = multer({ dest: "public/images" })
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const path = require("path");
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-// Config 
-
-app.set("view engine", "ejs")
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-//Rutas
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-    res.render("index")
-})
-
-// Rutas para subir Los archivos
-
-app.post("/upload", upload.single('imagen'), (req, res) => {
-    console.log(req.file)   
-    fs.renameSync(req.file.path, req.file.path + '.' + req.file.mimetype.split('/')[1]);
-    res.send("Archivo Subido")
-})
-
-app.post('/uploadmult', upload.array('imagenes'), (req, res) => {
-    console.log(req.files)
-    res.send('Termina!');
+  res.render("index");
 });
 
-app.listen(port)
+app.listen(port);
 
-console.log("App listen on Port " + port)
+console.log("App listen on Port " + port);
