9a3442e16eda9431a65d1f173f6d6a79e2f2ee4b
noeliadv <noeliadv@ext.inditex.com>
2023-01-17 11:38:07 -0300

    elimine la vista, agregue endpoint con respuesta

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 const express = require("express");
 const app = express();
-const fs = require("fs");
 const path = require("path");
 
 const port = process.env.PORT || 3000;
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/test", (req, res) => {
+  res.send("<h1>Pero mira que hermoso test</h1>");
 });
 
 app.listen(port);
 
-console.log("App listen on Port " + port);
+console.log("Escuchando en: http://localhost:" + port);
